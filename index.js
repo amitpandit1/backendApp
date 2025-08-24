54a4301b52a4b0220e44c48ef251e0f64d785c56
Amit Pandit <amitpandit.mail@gmail.com>
2025-08-24 14:12:40 +0530

    commit1

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+require("dotenv").config();
+const port = process.env.PORT;
+
+app.get("/", (req, res)=>{
+    res.send("<h1>HOME PAGE</h1>");
+})
+
+app.get("/login", (req, res)=>{
+    res.send("<h1>LOGIN PAGE</h1>");
+})
+
+app.listen(port, (req, res)=>{
+    console.log("Server is running");
+})
